f3c889e1806b7a3d95d6cd8b2f7c81b40645e8db
user4 <user4@DESKTOP-UF19NHE>
2023-11-21 11:30:40 +0100

    Ajoutez des fichiers projet.

---
+++ Tp1/Tp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tp1
+{
+    class Livre: IComparable <Livre>
+    {
+        public string titre;
+        public Livre(string t)
+        { 
+            titre = t;
+        }
+        public int comparer(Livre autre) 
+        {
+            return string.Compare(titre, autre.titre);
+        }
+
+        public int CompareTo(Livre? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tp1/Tp1/programme.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp1
+{
+    internal class programme
+    {
+        static void Main(string[] args)
+        {
+            List<Livre> Biblioth = new list<Livre>();
+            Livre livre1 = new Livre("c");
+            Livre livre2 = new Livre("a");
+            Livre livre3 = new Livre("b");
+
+            Biblioth.Add(livre1);
+            Biblioth.Add(livre2);
+            Biblioth.Add(livre3);
+
+            Biblioth.Sort();
+
+            foreach (Livre livre in Biblioth)
+            {
+                Console.WriteLine(livre.titre);
+            }
+        }
+    }
+}
